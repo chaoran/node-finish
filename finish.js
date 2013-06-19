9a489ffa65500dd1d221f72dcdea2d53e7c18007
Chaoran Yang <chaorany@me.com>
2013-06-18 23:48:17 -0500

    add .forEach, remove .ordered, .hashed, .reduce

--- finish.js
+++ finish.js
@@ -1,93 +1,48 @@
-var finish = function(finish, callback) {
-  var count = 0 
-    , end = false
-    , results = []
-    , error
-  
-  finish(function(func) {
-    if (error) return;
-    ++count
-    func(function(err, result) {
-      if (error) return
-      if (err) { error = err; return callback(err, results) }
-      
-      results.push(result)
-      
-      if (--count === 0 && end) {
-        callback(err, results)
-      }
-    })
-  })
-  end = true
+var Finish = function(callback) {
+  this.count = 0;
+  this.end = false;
+  this.results = [];
+  this.callback = callback;
 }
 
-finish.hashed = function(finish, callback) {
-  var count = 0
-    , end = false
-    , results = {}
-    , error 
+Finish.prototype.done = function(err, result) {
+  if (this.error) return;
+  if (err) { this.error = err; return this.callback(err); }
+  this.results.push(result);
+  if (--this.count === 0 && this.end) this.callback(null, this.results);
+}
+
+module.exports = function(body, callback) {
+  var finish = new Finish(callback);
+  var done = finish.done.bind(finish);
+
+  body(function(async) {
+    if (finish.error) return;
+    finish.count++;
+    async(done);
+  });
   
-  function keyedDone(key) {
-    return function(err, result) {
-      if (error) return;
-      if (err) { error = err; return callback(err, results) }
-      results[key] = result
-      if (--count === 0 && end) callback(err, results)
-    }
+  finish.end = true
+}
+
+module.exports.forEach = function(list, async, callback) {
+  var finish = new Finish(callback);
+  var done = finish.done.bind(finish);
+
+  finish.count = list.length;
+
+  switch (async.length) {
+    case 2: list.forEach(function(item) {
+      async(item, done);
+    }); break;
+    case 3: list.forEach(function(item, index) {
+      async(item, index, done);
+    }); break;
+    case 4: list.forEach(function(item, index, list) {
+      async(item, index, list, done);
+    }); break;
+    default: throw new Error('wrong number of arguments');
   }
 
-  finish(function(key, func) {
-    if (error) return;
-    ++count
-    func(keyedDone(key))
-  })
-  end = true
+  finish.end = true;
 }
-
-finish.ordered = function(finish, callback) {
-  var count = 0
-    , end = false
-    , results = []
-    , error
-  
-  function orderedDone(index) {
-    return function(err, result) {
-      if (error) return
-      if (err) { error = err; return callback(err, results) }
-      results[index] = result
-      if (--count === 0 && end) callback(err, results)
-    }
-  }
-  
-  finish(function(func) {
-    if (error) return;
-    func(orderedDone(count++))
-  })
-  end = true
-}
-
-finish.reduce = function(finish, reducer, callback) {
-  var count = 0
-    , end = false
-    , result
-    , error
-
-  finish(function(func) {
-    if (error) return;
-    ++count
-    func(function(err, res) {
-      if (error) return
-      if (err) { 
-        error = err 
-        return callback(err, result) 
-      }
-
-      result = result === undefined? res : reducer(result, res)
-
-      if (--count === 0 && end) callback(err, result)
-    })
-  })
-  end = true
-}
-
-module.exports = finish
